b3496748ab74676ca2890e32e8d2dd84c26a5af5
Karthik Raut <130137266+Karthikraut@users.noreply.github.com>
2025-04-27 22:52:21 +0530

    Time: 87 ms (76.63%) | Memory: 61.8 MB (97.95%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
         int n =nums.size();
+        int i=0,j=n-1;
         int ans=0;
-
-        for(int i=0;i<n;i++){
-            int a = k-nums[i];
-
-            if(mp.find(a)!=mp.end()){
-                mp[a]--;
-                if(mp[a]==0) mp.erase(a);
-                ans++;
-                continue;
+        sort(nums.begin(),nums.end());
+        while(i<j){
+            if(nums[i]+nums[j]>k){
+                j--;
             }
-            mp[nums[i]]+=1;
-        }
-
-        for(auto ele:mp){
-            cout<<ele.first<<" ";
+            else if(nums[i]+nums[j]<k){
+                i++;
+            }
+            else{
+                i++;
+                j--;
+                ans++;
+            }
         }
         return ans;
     }
