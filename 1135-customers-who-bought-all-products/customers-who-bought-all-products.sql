e20e5041d76258a13b04902eb92402ffb55c61c5
Karthik Raut <130137266+Karthikraut@users.noreply.github.com>
2025-07-21 15:03:56 +0530

    Time: 575 ms (45.58%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,4 +1,3 @@
-SELECT customer_id 
-FROM Customer 
-GROUP BY customer_id 
-HAVING COUNT(DISTINCT(product_key)) = (SELECT COUNT(product_key) FROM Product);
+SELECT customer_id
+FROM Customer C JOIN Product P ON C.product_key =P.product_key
+GROUP BY C.customer_id HAVING COUNT(DISTINCT(C.product_key)) = (SELECT COUNT(product_key) FROM Product)
