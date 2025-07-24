0c3675a82362617412124612fb916f06da9a424b
Karthik Raut <130137266+Karthikraut@users.noreply.github.com>
2025-07-24 23:39:38 +0530

    Time: 401 ms (75.51%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,5 +1,3 @@
-SELECT(
-    SELECT num FROM MyNumbers GROUP BY num having count(num) =1 ORDER  BY num DESC LIMIT 1 
-) AS num;
-
-
+SELECT CASE WHEN COUNT(num)=0 THEN null ELSE num END AS num FROM(
+    SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1 ORDER BY num DESC LIMIT 1
+) AS derived
