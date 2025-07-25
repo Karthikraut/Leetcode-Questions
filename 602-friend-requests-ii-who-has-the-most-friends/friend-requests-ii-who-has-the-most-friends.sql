036a562b6c7c5ab8554597b40dbba6b6ea70c115
Karthik Raut <130137266+Karthikraut@users.noreply.github.com>
2025-07-25 08:40:30 +0530

    Time: 288 ms (82.10%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,6 +1,5 @@
-SELECT id, COUNT(id) AS num FROM(
-    SELECT accepter_id as id FROM RequestAccepted  
-    UNION ALL 
-    SELECT requester_id FROM RequestAccepted
-) AS Cnt
-GROUP BY id ORDER BY COUNT(id) DESC LIMIT 1
+SELECT id, SUM(num) AS num FROM (
+    SELECT requester_id as id, COUNT(*) AS num FROM RequestAccepted GROUP BY requester_id
+UNION ALL SELECT accepter_id, COUNT(*) FROM RequestAccepted GROUP BY accepter_id
+) AS derived GROUP BY id ORDER BY num DESC LIMIT 1;
+
